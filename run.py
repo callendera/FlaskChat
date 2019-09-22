262aecc7df0d92c8043eee6187beec5677128652
Ubuntu <ubuntu@ip-172-31-45-91.us-east-2.compute.internal>
2019-09-22 22:02:46 +0000

    bug fix (more like UE)

--- run.py
+++ run.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    return "To send a message use /USERNAME/MESSAGE"
 
 
-@app.route("/<username>")
+@app.route('/<username>')
 def user(username):
     return "Hi " + username
 
 
-@app.route("/<username>/<message>")
+@app.route('/<username>/<message>')
 def send_message(username, message):
     return "{0}: {1}".format(username, message)
 
