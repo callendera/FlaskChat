97a525e9f13a1e9c4065767a916fdd897a0fa3d5
Ubuntu <ubuntu@ip-172-31-45-91.us-east-2.compute.internal>
2019-09-24 21:41:52 +0000

    quick save

--- run.py
+++ run.py
@@ -2,22 +2,29 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
 
 
-@app.route("/")
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
+
+
+@app.route('/')
 def index():
-    """Main page with instructions"""
+    """ Main page with instructions """
     return "To send a message use /USERNAME/MESSAGE"
 
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """Display messages"""
+    return "Welcome {0}".format(username)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """Create a new message and then redirect to chat page"""
     return "{0}: {1}".format(username, message)
 
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
